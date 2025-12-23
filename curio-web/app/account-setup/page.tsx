faeffa8695b15048bae2d0455552766143e0a3e8
Emily Song <esong@acuriolab.org>
2025-12-23 12:20:31 -0500

    Add curio-web next.js app

---
+++ curio-web/app/account-setup/page.tsx
@@ -0,0 +1,28 @@
+// local storage of user identity is handled here
+
+
+"use client";
+
+import { useEffect } from "react";
+import { LoginPage } from "../components/LoginPage";
+
+export default function AccountSetupPage() {
+  useEffect(() => {
+    // If already logged in, go straight to projects
+    const storedUser = localStorage.getItem("currentUser");
+    if (storedUser) {
+      window.location.href = "/projects";
+    }
+  }, []);
+
+  const handleLogin = (user: { email: string; name: string }) => {
+    localStorage.setItem("currentUser", JSON.stringify(user));
+    window.location.href = "/projects";
+  };
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+      <LoginPage onLogin={handleLogin} />
+    </div>
+  );
+}
